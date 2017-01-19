6983042a4eff39258359314d84770d348c1bcf73
Vladimir Kazanov <vkazanov@inbox.ru>
2017-01-19 17:48:41 +0000

    Automatically stop Elfuse when Emacs gets killed

--- elfuse.el
+++ elfuse.el
@@ -4,28 +4,33 @@
 (defconst elfuse--time-between-checks 0.01
   "Time interval in seconds between Elfuse request checks.")
 
+(defvar elfuse--check-timer nil
+  "Timer calling the callback-responding function")
+
 (defun elfuse-start (mountpath)
   "Start Elfuse using a given MOUNTPATH."
   (interactive "DElfuse mount path: ")
   (if (elfuse--dir-mountable-p mountpath)
       (let ((abspath (file-truename mountpath)))
 	(elfuse--start-loop)
-	(elfuse--mount abspath))
+	(elfuse--mount abspath)
+        (add-hook 'kill-emacs-hook 'elfuse--stop))
     (message "Elfuse: %s does not exist or is not empty." mountpath)))
 
 (defun elfuse-stop ()
   "Stop Elfuse."
   (interactive)
-  (elfuse--stop))
+  (elfuse--stop)
+  (remove-hook 'kill-emacs-hook 'elfuse--stop))
 
 (defun elfuse--start-loop ()
-  (let ((timer (run-at-time
-                nil
-                elfuse--time-between-checks
-                (lambda ()
-		  (unless (elfuse--check-callbacks)
-		    (cancel-timer timer))))))))
+  (setq elfuse--check-timer
+        (run-at-time nil elfuse--time-between-checks 'elfuse--on-timer)))
 
+(defun elfuse--on-timer ()
+  (unless (elfuse--check-callbacks)
+    (cancel-timer timer)
+    (setq elfuse--check-timer nil)))
 
 (defun elfuse--dir-mountable-p (path)
   (and (file-exists-p path)
@@ -94,4 +99,3 @@
    ((equal path "/other") 0)
    ((equal path "/etc") 0)
    (t -1)))
-
